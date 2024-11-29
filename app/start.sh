06a2b4fbdfc0b49c0c5698842e7cadd92ef29e1d
Hangbin Liu <liuhangbin@gmail.com>
2024-11-29 19:19:05 +0800

    app: fix dav not start issue

--- app/start.sh
+++ app/start.sh
@@ -2,10 +2,12 @@
 
 source /myenv/bin/activate
 while true; do
-	if [ -z "${DAV_PORT}" ] && [ -f /data/fast115.sqlite ] && [ -f /data/115-cookies.txt ]; then
-		[ -z "${FAST_STRM}" ] && param='-fs' || param=""
-		servedb dav -cp /data/115-cookies.txt -f /data/fast115.sqlite \
-			$param -P ${DAV_PORT} &> /tmp/dav.log &
+	if [ -n "${DAV_PORT}" ] && [ -f /data/fast115.sqlite ] && [ -f /data/115-cookies.txt ]; then
+		param="-P ${DAV_PORT}"
+		[ -n "${FAST_STRM}" ] && param="${param} -fs"
+		[ -n "${STRM_HOST}" ] && param="${param} -o ${STRM_HOST}"
+		servedb dav -cp /data/115-cookies.txt -f /data/fast115.sqlite $param &> /data/dav.log &
 	fi
 	/app/app.py &> /data/app.log
+	sleep 10
 done
